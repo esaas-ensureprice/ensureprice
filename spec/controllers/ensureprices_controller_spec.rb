22127cf8e8550829d2a5ae1a226212bd168b734a
Yukti Khurana <yuktikhurana99@gmail.com>
2022-10-30 16:40:40 -0400

    rspec tests for controller and factory bot models

--- spec/controllers/ensureprices_controller_spec.rb
+++ spec/controllers/ensureprices_controller_spec.rb
@@ -1,13 +1,68 @@
 require 'rails_helper'
+require 'spec_helper'
 
-describe EnsurepricesController, :type => :controller do
-  let!(:insurance_plan1) {FactoryBot.create(:insurance_plans, company_name: 'Company1', insurance_plan_name: 'Plan1', individual_annual_deductible: '5000')}
-  let!(:insurance_plan2) {FactoryBot.create(:insurance_plans, company_name: 'Company2', insurance_plan_name: 'Plan2', uc: '40')}
+RSpec.describe EnsurepricesController, :type => :controller do
+  let!(:insurance_plan1) {FactoryBot.create(:insurance_plans, company_name: 'Company1', insurance_plan_name: 'PLAN1', individual_annual_deductible: '5000')}
+  let!(:insurance_plan2) {FactoryBot.create(:insurance_plans, company_name: 'Company2', insurance_plan_name: 'PLAN2', uc: '40')}
+  let!(:insurance_plan3) {FactoryBot.create(:insurance_plans, company_name: 'Company1', insurance_plan_name: 'PLAN3')}
 
+  let!(:doctor1) {FactoryBot.create(:doctors, doctor_name: 'Dr. Yukti', insurance_plan: "Company1")}
+  let!(:doctor2) {FactoryBot.create(:doctors, doctor_name: 'Dr. Jo', insurance_plan: "Company1")}
+  let!(:doctor3) {FactoryBot.create(:doctors, doctor_name: 'Dr. Muhan', insurance_plan: "Company2")}
+      
   describe 'GET show' do
+    it 'should assign insurance providers variable correctly' do
+      get :show, id: insurance_plan1.id
+      expected_result = ['Company1', 'Company2']
+      expect(assigns(:insurance_providers)).to eq(expected_result)
+    end
+
     it 'should render the show template' do
-        get :show, id: insurance_plan1.id
-        expect(response).to render_template('show')
+      get :show, id: insurance_plan1.id
+      expect(response).to render_template('show')
+    end
+  end
+
+  describe 'GET plans' do
+    it 'should assign insurance plans correctly' do
+      get :plans, id: insurance_plan1.company_name
+      request.session[:id] = "Company1"
+      expected_result = [insurance_plan1.insurance_plan_name, insurance_plan3.insurance_plan_name]
+      expect(assigns(:insurance_plans)).to eq(expected_result)
+    end
+
+    it 'should render the plans template' do
+      get :plans, id: insurance_plan1.company_name
+      expect(response).to render_template('plans')
+    end
+  end
+
+  describe 'GET plans' do
+    it 'should assign insurance plans correctly' do
+      get :plans, id: insurance_plan1.company_name
+      request.session[:id] = "Company1"
+      expected_result = [insurance_plan1.insurance_plan_name, insurance_plan3.insurance_plan_name]
+      expect(assigns(:insurance_plans)).to eq(expected_result)
+    end
+
+    it 'should render the plans template' do
+      get :plans, id: insurance_plan1.company_name
+      expect(response).to render_template('plans')
+    end
+  end
+
+  describe 'GET doctors' do
+    # DELETE
+    # it 'should assign doctors correctly' do
+    #   get :doctors, id: insurance_plan1.insurance_plan_name
+    #   request.session[:id] = insurance_plan1.company_name
+    #   expected_result = [doctor1.doctor_name, doctor2.doctor_name]
+    #   expect(assigns(:doctors)).to eq(expected_result)
+    # end
+
+    it 'should render the doctors template' do
+      get :doctors, id: insurance_plan1.insurance_plan_name
+      expect(response).to render_template('doctors')
     end
   end
 end
