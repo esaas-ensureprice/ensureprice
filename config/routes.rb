f09c134271927990a11518dbd4643950f63156a0
Josephine Chan <jcf1996chan@gmail.com>
2022-11-02 18:55:12 -0400

    Error fix: changed route name to fix error in session on Google Chrome

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Ensureprice::Application.routes.draw do
   resources :ensureprices
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/ensureprices')
-  get '/:id', to: 'ensureprices#plans', as: 'plans'
+  get 'plans/:id', to: 'ensureprices#plans', as: 'plans'
   get 'doctors/:id', to: 'ensureprices#doctors', as: 'doctors'
   get 'visits/:id', to: 'ensureprices#visits', as: 'visits'
   get 'price/:id', to: 'ensureprices#price', as: 'price'
