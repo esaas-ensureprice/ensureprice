aa4c04efaa0a384c1a2f46605807228a848a344c
Yukti Khurana <yuktikhurana99@gmail.com>
2022-11-11 00:34:46 -0500

    fixing application routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 Ensureprice::Application.routes.draw do
-  resources :ensureprices
-  # map '/' to be a redirect to '/movies'
-  root :to => redirect('/ensureprices')
+  # Main Application routes
+  # map '/' to be a redirect to '/ensureprices' site
+  # root :to => redirect('/ensureprices')
+
+  # New home page
+  root 'static_pages#home'
+  get 'help', to: 'static_pages#help'
+  get 'about', to: 'static_pages#about'
+
+  # New User signup and login routes
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  # Main Application routes
   get 'plans/:id', to: 'ensureprices#plans', as: 'plans'
   get 'doctors/:id', to: 'ensureprices#doctors', as: 'doctors'
   get 'visits/:id', to: 'ensureprices#visits', as: 'visits'
   get 'price/:id', to: 'ensureprices#price', as: 'price'
+
+  resources :users
+  resources :ensureprices
 end
