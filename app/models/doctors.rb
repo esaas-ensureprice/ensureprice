de6e23e8364b58dba10c9ab7173a6ca0d11d3c70
Yukti Khurana <yuktikhurana99@gmail.com>
2022-11-14 23:06:04 -0500

    removing extra function

--- app/models/doctors.rb
+++ app/models/doctors.rb
@@ -2,8 +2,4 @@ class Doctors < ActiveRecord::Base
     def self.get_in_network_doctors insurance_provider
         return Doctors.where(insurance_plan: insurance_provider)
     end
-
-    def self.with_insurance_plans(plans_list)
-        Doctors.where(insurance_plan: plans_list)
-      end
 end
