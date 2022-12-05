240b45dee4180c58d2a77d07ccc8a2abe8699217
Josephine Chan <jcf1996chan@gmail.com>
2022-12-05 17:47:28 -0500

    Fixing profile pic heroku issues

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -36,7 +36,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-     %w(jpg jpeg gif png)
+     %w(png)
   end
 
   # Override the filename of the uploaded files:
@@ -44,4 +44,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  def size_range
+    0.byte..1.megabytes
+  end
 end
