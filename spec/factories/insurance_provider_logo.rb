f1112c986a7a20ab67458ed745b030a2c79265af
Yukti Khurana <yuktikhurana99@gmail.com>
2022-12-19 04:26:17 -0500

    RSpec Tests: Insurance Provider Logo Model

---
+++ spec/factories/insurance_provider_logo.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+    factory :insurance_provider_logo do
+      company_name { 'Aetna' }
+      logo {'https://www.aetnainternational.com/content/dam/aetna/images/icons/1_Aetna_StorytellingIcon_Caring_Violet.png'}
+    end
+  end
